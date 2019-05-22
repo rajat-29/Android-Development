3a5b1ae565c099f2fcd84a7f20a64bbc1c1a70cd
rajat-29 <rajat295gupta@gmail.com>
2019-05-22 09:32:49 +0530

    media player

---
+++ MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.aajkalteremerepyaarkecharche);
+
+        Button playButton = (Button)findViewById(R.id.play_btn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this,"Done",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        Button pauseButton = (Button)findViewById(R.id.pause_btn);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
